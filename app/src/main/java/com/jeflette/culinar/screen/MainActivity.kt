f0de09fa949ad726e2964387c821ed85bc1f2873
dionpouw <aldion.sumampouw@gmail.com>
2022-10-24 23:07:39 +0800

    move the activity to other package

--- app/src/main/java/com/jeflette/culinar/screen/MainActivity.kt
+++ app/src/main/java/com/jeflette/culinar/screen/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.jeflette.culinar
+package com.jeflette.culinar.screen
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -6,10 +6,11 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import com.jeflette.culinar.screen.homescreen.HomeScreenApp
 import com.jeflette.culinar.ui.theme.CulinarTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,25 +20,25 @@ class MainActivity : ComponentActivity() {
             CulinarTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    HomeScreenApp()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, device = Devices.PIXEL_3, showSystemUi = true)
 @Composable
 fun DefaultPreview() {
     CulinarTheme {
-        Greeting("Android")
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
+        ) {
+            HomeScreenApp()
+        }
     }
 }
