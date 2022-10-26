47b42e1c72957c69972117acf3169e96cd507812
dionpouw <aldion.sumampouw@gmail.com>
2022-10-26 20:37:49 +0800

    added hitlandroidapp

--- app/src/main/java/com/jeflette/culinar/CulinarApplication.kt
+++ app/src/main/java/com/jeflette/culinar/CulinarApplication.kt
@@ -1,5 +1,8 @@
 package com.jeflette.culinar
 
 import android.app.Application
+import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class CulinarApplication : Application()
