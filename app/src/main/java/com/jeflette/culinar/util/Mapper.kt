170260b1f06401339c67f4f82d2146e239f9a0eb
dionpouw <aldion.sumampouw@gmail.com>
2022-10-26 20:39:10 +0800

    mapper to list restaurant

---
+++ app/src/main/java/com/jeflette/culinar/util/Mapper.kt
@@ -0,0 +1,56 @@
+package com.jeflette.culinar.util
+
+import com.jeflette.culinar.data.remote.response.Restaurant
+import com.jeflette.culinar.data.remote.response.Restaurants
+
+fun mapFromRestaurantsToList(restaurants: Restaurants): List<Restaurant> {
+    val restaurantMap = listOf(
+        Restaurant(
+            name = restaurants.restaurant1?.name,
+            rating = restaurants.restaurant1?.rating,
+            imageUrl = restaurants.restaurant1?.imageUrl,
+            averagePrice = restaurants.restaurant1?.averagePrice,
+        ), Restaurant(
+            name = restaurants.restaurant2?.name,
+            rating = restaurants.restaurant2?.rating,
+            imageUrl = restaurants.restaurant2?.imageUrl,
+            averagePrice = restaurants.restaurant2?.averagePrice,
+        ), Restaurant(
+            name = restaurants.restaurant3?.name,
+            rating = restaurants.restaurant3?.rating,
+            imageUrl = restaurants.restaurant3?.imageUrl,
+            averagePrice = restaurants.restaurant3?.averagePrice,
+        ), Restaurant(
+            name = restaurants.restaurant4?.name,
+            rating = restaurants.restaurant4?.rating,
+            imageUrl = restaurants.restaurant4?.imageUrl,
+            averagePrice = restaurants.restaurant4?.averagePrice,
+        ), Restaurant(
+            name = restaurants.restaurant5?.name,
+            rating = restaurants.restaurant5?.rating,
+            imageUrl = restaurants.restaurant5?.imageUrl,
+            averagePrice = restaurants.restaurant5?.averagePrice,
+        ), Restaurant(
+            name = restaurants.restaurant6?.name,
+            rating = restaurants.restaurant6?.rating,
+            imageUrl = restaurants.restaurant6?.imageUrl,
+            averagePrice = restaurants.restaurant6?.averagePrice,
+        ), Restaurant(
+            name = restaurants.restaurant7?.name,
+            rating = restaurants.restaurant7?.rating,
+            imageUrl = restaurants.restaurant7?.imageUrl,
+            averagePrice = restaurants.restaurant7?.averagePrice,
+        ), Restaurant(
+            name = restaurants.restaurant8?.name,
+            rating = restaurants.restaurant8?.rating,
+            imageUrl = restaurants.restaurant8?.imageUrl,
+            averagePrice = restaurants.restaurant8?.averagePrice,
+        ), Restaurant(
+            name = restaurants.restaurant9?.name,
+            rating = restaurants.restaurant9?.rating,
+            imageUrl = restaurants.restaurant9?.imageUrl,
+            averagePrice = restaurants.restaurant9?.averagePrice,
+        )
+    )
+    return restaurantMap
+}
