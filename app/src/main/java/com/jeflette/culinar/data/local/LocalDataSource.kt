5b57e492a59c6b5cc1e94bb7abe3668dfadd2a17
dionpouw <aldion.sumampouw@gmail.com>
2022-10-26 20:41:06 +0800

    add local & remote data source

--- app/src/main/java/com/jeflette/culinar/data/local/LocalDataSource.kt
+++ app/src/main/java/com/jeflette/culinar/data/local/LocalDataSource.kt
@@ -1,4 +1,10 @@
 package com.jeflette.culinar.data.local
 
-class LocalDataSource {
+import com.jeflette.culinar.data.local.db.RestaurantDao
+import javax.inject.Inject
+
+class LocalDataSource @Inject constructor(
+    private val restaurantDao: RestaurantDao
+) {
+    suspend fun getRestaurantList() = restaurantDao.getRestaurantList()
 }
--- app/src/main/java/com/jeflette/culinar/data/remote/RemoteDataSource.kt
+++ app/src/main/java/com/jeflette/culinar/data/remote/RemoteDataSource.kt
@@ -1,4 +1,14 @@
 package com.jeflette.culinar.data.remote
 
-class RemoteDataSource {
+import com.jeflette.culinar.data.remote.response.Mood
+import com.jeflette.culinar.network.ApiService
+import javax.inject.Inject
+
+class RemoteDataSource @Inject constructor(
+    private val apiService: ApiService
+) {
+
+    suspend fun getRestaurantList(data: Mood) = apiService.getRestaurantList(data)
+
+
 }
