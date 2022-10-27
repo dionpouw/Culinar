792999ed6daab73c374c4c1852c605186e001e90
dionpouw <aldion.sumampouw@gmail.com>
2022-10-27 22:26:08 +0800

    add func to save to database

--- app/src/main/java/com/jeflette/culinar/screen/homescreen/HomeScreenViewModel.kt
+++ app/src/main/java/com/jeflette/culinar/screen/homescreen/HomeScreenViewModel.kt
@@ -1,13 +1,15 @@
 package com.jeflette.culinar.screen.homescreen
 
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.jeflette.culinar.data.RestaurantRepository
+import com.jeflette.culinar.data.local.entity.Restaurant
 import com.jeflette.culinar.data.remote.response.Mood
 import com.jeflette.culinar.data.remote.response.Restaurants
+import com.jeflette.culinar.util.mapFromRestaurantsToList
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -16,8 +18,11 @@ class HomeScreenViewModel @Inject constructor(
     private val repository: RestaurantRepository
 ) : ViewModel() {
 
-    private val _restaurantList: MutableState<Restaurants> = mutableStateOf(Restaurants())
-    val restaurantList: MutableState<Restaurants> = _restaurantList
+    // TODO Fix this nullability
+    private val _restaurantList = MutableStateFlow(Restaurants())
+    val restaurantList: StateFlow<Restaurants> = _restaurantList
+
+    lateinit var restaurant: List<Restaurant>
 
     init {
         getRestaurantFromRemote()
@@ -28,6 +33,14 @@ class HomeScreenViewModel @Inject constructor(
             _restaurantList.value =
                 repository.getRestaurantListFromRemote(Mood("happy")).data?.restaurants
                     ?: Restaurants()
+            restaurant = mapFromRestaurantsToList(restaurantList.value)
+            saveRestaurantsToDatabase(restaurants = restaurant)
+        }
+    }
+
+    fun saveRestaurantsToDatabase(restaurants: List<Restaurant>) {
+        viewModelScope.launch {
+            repository.saveRestaurantsToDb(restaurants)
         }
     }
 }
