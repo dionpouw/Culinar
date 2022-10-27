f97440ddf883479b380d6bc6f090540fc1e1bdbe
dionpouw <aldion.sumampouw@gmail.com>
2022-10-27 22:26:15 +0800

    add local data source

--- app/src/main/java/com/jeflette/culinar/data/local/LocalDataSource.kt
+++ app/src/main/java/com/jeflette/culinar/data/local/LocalDataSource.kt
@@ -1,10 +1,14 @@
 package com.jeflette.culinar.data.local
 
 import com.jeflette.culinar.data.local.db.RestaurantDao
+import com.jeflette.culinar.data.local.entity.Restaurant
 import javax.inject.Inject
 
 class LocalDataSource @Inject constructor(
     private val restaurantDao: RestaurantDao
 ) {
     suspend fun getRestaurantList() = restaurantDao.getRestaurantList()
+
+    suspend fun saveRestaurantsToDb(restaurantList: List<Restaurant>) =
+        restaurantDao.saveRestaurantsToDb(restaurantList)
 }
