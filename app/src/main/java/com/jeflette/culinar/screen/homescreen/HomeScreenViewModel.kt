d9978f0f951f207ae5487d9380e6312f76b30336
dionpouw <aldion.sumampouw@gmail.com>
2022-10-30 21:37:40 +0800

    using stateflow for get the list of restaurants

--- app/src/main/java/com/jeflette/culinar/screen/homescreen/HomeScreenViewModel.kt
+++ app/src/main/java/com/jeflette/culinar/screen/homescreen/HomeScreenViewModel.kt
@@ -4,9 +4,6 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.jeflette.culinar.data.RestaurantRepository
 import com.jeflette.culinar.data.local.entity.Restaurant
-import com.jeflette.culinar.data.remote.response.Mood
-import com.jeflette.culinar.data.remote.response.Restaurants
-import com.jeflette.culinar.util.mapFromRestaurantsToList
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -18,29 +15,14 @@ class HomeScreenViewModel @Inject constructor(
     private val repository: RestaurantRepository
 ) : ViewModel() {
 
-    // TODO Fix this nullability
-    private val _restaurantList = MutableStateFlow(Restaurants())
-    val restaurantList: StateFlow<Restaurants> = _restaurantList
+    private val _restaurantList = MutableStateFlow<List<Restaurant>>(emptyList())
+    val restaurantList: StateFlow<List<Restaurant>> = _restaurantList
 
-    lateinit var restaurant: List<Restaurant>
-
-    init {
-        getRestaurantFromRemote()
-    }
-
-    private fun getRestaurantFromRemote() {
+    fun getRestaurantList(mood: String) {
         viewModelScope.launch {
-            _restaurantList.value =
-                repository.getRestaurantListFromRemote(Mood("happy")).data?.restaurants
-                    ?: Restaurants()
-            restaurant = mapFromRestaurantsToList(restaurantList.value)
-            saveRestaurantsToDatabase(restaurants = restaurant)
-        }
-    }
-
-    fun saveRestaurantsToDatabase(restaurants: List<Restaurant>) {
-        viewModelScope.launch {
-            repository.saveRestaurantsToDb(restaurants)
+            repository.getRestaurant(mood).collect {
+                _restaurantList.value = it.data ?: emptyList()
+            }
         }
     }
 }
