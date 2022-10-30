5db4246545768de3e790a39a998d562625d9d2ff
dionpouw <aldion.sumampouw@gmail.com>
2022-10-30 21:36:35 +0800

    Using NetworkResourceBound and Resource sealed class

--- app/src/main/java/com/jeflette/culinar/data/RestaurantRepository.kt
+++ app/src/main/java/com/jeflette/culinar/data/RestaurantRepository.kt
@@ -1,19 +1,26 @@
 package com.jeflette.culinar.data
 
 import com.jeflette.culinar.data.local.LocalDataSource
-import com.jeflette.culinar.data.local.entity.Restaurant
 import com.jeflette.culinar.data.remote.RemoteDataSource
 import com.jeflette.culinar.data.remote.response.Mood
+import com.jeflette.culinar.data.remote.response.Restaurants
+import com.jeflette.culinar.util.mapFromRestaurantsToList
+import com.jeflette.culinar.util.networkBoundResource
 import javax.inject.Inject
 
 class RestaurantRepository @Inject constructor(
     private val remoteDataSource: RemoteDataSource, private val localDataSource: LocalDataSource
 ) {
 
-    suspend fun getRestaurantListFromRemote(data: Mood) = remoteDataSource.getRestaurantList(data)
-
-    suspend fun getRestaurantListFromLocal() = localDataSource.getRestaurantList()
-
-    suspend fun saveRestaurantsToDb(restaurantList: List<Restaurant>) =
-        localDataSource.saveRestaurantsToDb(restaurantList)
+    fun getRestaurant(mood: String) = networkBoundResource(
+        query = { localDataSource.getRestaurantList(mood) },
+        fetch = { remoteDataSource.getRestaurantList(Mood(mood)) },
+        saveFetchResult = {
+            localDataSource.saveRestaurantsToDb(
+                mapFromRestaurantsToList(
+                    it.data?.restaurants ?: Restaurants()
+                )
+            )
+        },
+    )
 }
