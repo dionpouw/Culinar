53061b998a4108c780d9be3e2e35a3709d8fdad5
dionpouw <aldion.sumampouw@gmail.com>
2022-10-30 21:36:14 +0800

    Using Flow for NetworkResourceBound

--- app/src/main/java/com/jeflette/culinar/data/local/db/RestaurantDao.kt
+++ app/src/main/java/com/jeflette/culinar/data/local/db/RestaurantDao.kt
@@ -5,16 +5,15 @@ import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.jeflette.culinar.data.local.entity.Restaurant
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface RestaurantDao {
 
+    // TODO Add a mood into the database
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun saveRestaurantsToDb(restaurant: List<Restaurant>)
 
-    @Query("SELECT * FROM restaurants")
-    suspend fun getRestaurantList(): List<Restaurant>
-
-
-// TODO : Add function to get all restaurant from database as MutableStateFlow
+    @Query("SELECT * FROM restaurants where mood = :mood")
+    fun getRestaurantList(mood: String): Flow<List<Restaurant>>
 }
