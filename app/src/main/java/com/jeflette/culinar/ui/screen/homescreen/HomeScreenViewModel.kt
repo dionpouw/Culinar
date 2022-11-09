f20bd666ceaf9d8306cc343f89d77298dc11fea6
dionpouw <aldion.sumampouw@gmail.com>
2022-11-09 22:52:41 +0800

    adding selected function for chip

--- app/src/main/java/com/jeflette/culinar/ui/screen/homescreen/HomeScreenViewModel.kt
+++ app/src/main/java/com/jeflette/culinar/ui/screen/homescreen/HomeScreenViewModel.kt
@@ -1,9 +1,10 @@
-package com.jeflette.culinar.screen.homescreen
+package com.jeflette.culinar.ui.screen.homescreen
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.jeflette.culinar.data.RestaurantRepository
 import com.jeflette.culinar.data.local.entity.Restaurant
+import com.jeflette.culinar.util.MoodState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -15,14 +16,26 @@ class HomeScreenViewModel @Inject constructor(
     private val repository: RestaurantRepository
 ) : ViewModel() {
 
+    private val _moodState: MutableStateFlow<String> =
+        MutableStateFlow(MoodState.Happy.name.lowercase())
+    val moodState: StateFlow<String> = _moodState
+
     private val _restaurantList = MutableStateFlow<List<Restaurant>>(emptyList())
     val restaurantList: StateFlow<List<Restaurant>> = _restaurantList
 
-    fun getRestaurantList(mood: String) {
+    init {
+        getRestaurantList()
+    }
+
+    fun getRestaurantList() {
         viewModelScope.launch {
-            repository.getRestaurant(mood).collect {
+            repository.getRestaurant(moodState.value.lowercase()).collect {
                 _restaurantList.value = it.data ?: emptyList()
             }
         }
+    }
+
+    fun onSelectedCategoryChanged(category: String) {
+        _moodState.value = category
     }
 }
