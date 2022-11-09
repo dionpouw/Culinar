48fb9ef9ba820ecff7582f3c9c711192ed8db5b9
dionpouw <aldion.sumampouw@gmail.com>
2022-11-09 22:54:24 +0800

    mapper from json to entity

--- app/src/main/java/com/jeflette/culinar/util/Mapper.kt
+++ app/src/main/java/com/jeflette/culinar/util/Mapper.kt
@@ -2,13 +2,16 @@ package com.jeflette.culinar.util
 
 import com.jeflette.culinar.data.local.entity.Restaurant
 import com.jeflette.culinar.data.remote.response.Restaurants
+import java.text.NumberFormat
+import java.util.*
 
-fun mapFromRestaurantsToList(restaurants: Restaurants): List<Restaurant> {
+fun mapFromRestaurantsToList(restaurants: Restaurants, mood: String): List<Restaurant> {
     return listOf(
         Restaurant(
             name = restaurants.restaurant1?.name,
-            address = restaurants.restaurant1?.address,
+            address = restaurants.restaurant1?.address ?: "",
             rating = restaurants.restaurant1?.rating,
+            mood = mood,
             imageUrl = restaurants.restaurant1?.imageUrl,
             averagePrice = restaurants.restaurant1?.averagePrice,
             latitude = restaurants.restaurant1?.latitude,
@@ -16,8 +19,9 @@ fun mapFromRestaurantsToList(restaurants: Restaurants): List<Restaurant> {
             cuisine = restaurants.restaurant1?.cuisine
         ), Restaurant(
             name = restaurants.restaurant2?.name,
-            address = restaurants.restaurant2?.address,
+            address = restaurants.restaurant2?.address ?: "",
             rating = restaurants.restaurant2?.rating,
+            mood = mood,
             imageUrl = restaurants.restaurant2?.imageUrl,
             averagePrice = restaurants.restaurant2?.averagePrice,
             latitude = restaurants.restaurant2?.latitude,
@@ -25,7 +29,8 @@ fun mapFromRestaurantsToList(restaurants: Restaurants): List<Restaurant> {
             cuisine = restaurants.restaurant2?.cuisine
         ), Restaurant(
             name = restaurants.restaurant3?.name,
-            address = restaurants.restaurant3?.address,
+            address = restaurants.restaurant3?.address ?: "",
+            mood = mood,
             rating = restaurants.restaurant3?.rating,
             imageUrl = restaurants.restaurant3?.imageUrl,
             averagePrice = restaurants.restaurant3?.averagePrice,
@@ -34,7 +39,8 @@ fun mapFromRestaurantsToList(restaurants: Restaurants): List<Restaurant> {
             cuisine = restaurants.restaurant3?.cuisine
         ), Restaurant(
             name = restaurants.restaurant4?.name,
-            address = restaurants.restaurant4?.address,
+            address = restaurants.restaurant4?.address ?: "",
+            mood = mood,
             rating = restaurants.restaurant4?.rating,
             imageUrl = restaurants.restaurant4?.imageUrl,
             averagePrice = restaurants.restaurant4?.averagePrice,
@@ -43,7 +49,8 @@ fun mapFromRestaurantsToList(restaurants: Restaurants): List<Restaurant> {
             cuisine = restaurants.restaurant4?.cuisine
         ), Restaurant(
             name = restaurants.restaurant5?.name,
-            address = restaurants.restaurant5?.address,
+            address = restaurants.restaurant5?.address ?: "",
+            mood = mood,
             rating = restaurants.restaurant5?.rating,
             imageUrl = restaurants.restaurant5?.imageUrl,
             averagePrice = restaurants.restaurant5?.averagePrice,
@@ -52,7 +59,8 @@ fun mapFromRestaurantsToList(restaurants: Restaurants): List<Restaurant> {
             cuisine = restaurants.restaurant5?.cuisine
         ), Restaurant(
             name = restaurants.restaurant6?.name,
-            address = restaurants.restaurant6?.address,
+            address = restaurants.restaurant6?.address ?: "",
+            mood = mood,
             rating = restaurants.restaurant6?.rating,
             imageUrl = restaurants.restaurant6?.imageUrl,
             averagePrice = restaurants.restaurant6?.averagePrice,
@@ -61,7 +69,8 @@ fun mapFromRestaurantsToList(restaurants: Restaurants): List<Restaurant> {
             cuisine = restaurants.restaurant6?.cuisine
         ), Restaurant(
             name = restaurants.restaurant7?.name,
-            address = restaurants.restaurant7?.address,
+            address = restaurants.restaurant7?.address ?: "",
+            mood = mood,
             rating = restaurants.restaurant7?.rating,
             imageUrl = restaurants.restaurant7?.imageUrl,
             averagePrice = restaurants.restaurant7?.averagePrice,
@@ -70,7 +79,8 @@ fun mapFromRestaurantsToList(restaurants: Restaurants): List<Restaurant> {
             cuisine = restaurants.restaurant7?.cuisine
         ), Restaurant(
             name = restaurants.restaurant8?.name,
-            address = restaurants.restaurant8?.address,
+            address = restaurants.restaurant8?.address ?: "",
+            mood = mood,
             rating = restaurants.restaurant8?.rating,
             imageUrl = restaurants.restaurant8?.imageUrl,
             averagePrice = restaurants.restaurant8?.averagePrice,
@@ -79,13 +89,22 @@ fun mapFromRestaurantsToList(restaurants: Restaurants): List<Restaurant> {
             cuisine = restaurants.restaurant8?.cuisine
         ), Restaurant(
             name = restaurants.restaurant9?.name,
-            address = restaurants.restaurant9?.address,
+            address = restaurants.restaurant9?.address ?: "",
             rating = restaurants.restaurant9?.rating,
             imageUrl = restaurants.restaurant9?.imageUrl,
+            mood = mood,
             averagePrice = restaurants.restaurant9?.averagePrice,
             latitude = restaurants.restaurant9?.latitude,
             longitude = restaurants.restaurant9?.longitude,
             cuisine = restaurants.restaurant9?.cuisine
         )
     )
+}
+
+fun String.toCurrencyFormat(): String {
+    val localeID = Locale("in", "ID")
+    val doubleValue = this.toDoubleOrNull() ?: return this
+    val numberFormat = NumberFormat.getCurrencyInstance(localeID)
+    numberFormat.minimumFractionDigits = 0
+    return numberFormat.format(doubleValue)
 }
