d7b811ba8d2f542976b235d09b9a7e5254f49aaa
dionpouw <aldion.sumampouw@gmail.com>
2022-11-09 22:55:55 +0800

    restaurant dao

--- app/src/main/java/com/jeflette/culinar/data/local/db/RestaurantDao.kt
+++ app/src/main/java/com/jeflette/culinar/data/local/db/RestaurantDao.kt
@@ -10,10 +10,12 @@ import kotlinx.coroutines.flow.Flow
 @Dao
 interface RestaurantDao {
 
-    // TODO Add a mood into the database
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun saveRestaurantsToDb(restaurant: List<Restaurant>)
 
     @Query("SELECT * FROM restaurants where mood = :mood")
     fun getRestaurantList(mood: String): Flow<List<Restaurant>>
+
+    @Query("SELECT * FROM restaurants where address = :address")
+    fun getRestaurantByAddress(address: String): Flow<Restaurant>
 }
