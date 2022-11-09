5ec03e3c2c2b9679bb305656b8fe9c0c4e20df89
dionpouw <aldion.sumampouw@gmail.com>
2022-11-09 22:32:04 +0800

    cleanup

--- app/src/main/java/com/jeflette/culinar/screen/detailscreen/DetailScreenContent.kt
+++
@@ -1,7 +0,0 @@
-package com.jeflette.culinar.screen.detailscreen
-
-import androidx.compose.runtime.Composable
-
-@Composable
-fun DetailScreenContent() {
-}
--- app/src/main/java/com/jeflette/culinar/util/NavigationState.kt
+++
@@ -1,6 +0,0 @@
-package com.jeflette.culinar.util
-
-enum class NavigationState {
-    HOME,
-    DETAIL,
-}
