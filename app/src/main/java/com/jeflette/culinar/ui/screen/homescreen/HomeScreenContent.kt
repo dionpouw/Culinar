353cadbd25f06a7a4dfb11ae851446fd57e706be
dionpouw <aldion.sumampouw@gmail.com>
2022-11-09 22:33:15 +0800

    HomeScreenContent for Column, Category row, and Lazy column

--- app/src/main/java/com/jeflette/culinar/ui/screen/homescreen/HomeScreenContent.kt
+++ app/src/main/java/com/jeflette/culinar/ui/screen/homescreen/HomeScreenContent.kt
@@ -1,4 +1,4 @@
-package com.jeflette.culinar.screen.commonui
+package com.jeflette.culinar.ui.screen.homescreen
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
@@ -7,83 +7,95 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Star
-import androidx.compose.material3.Card
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.jeflette.culinar.data.local.entity.Restaurant
-import com.jeflette.culinar.screen.homescreen.HomeScreenViewModel
+import com.jeflette.culinar.ui.screen.commonui.MoodCategoryChip
 import com.jeflette.culinar.ui.theme.interFamily
-import com.jeflette.culinar.util.MoodState
 import com.jeflette.culinar.util.fakeMood
 
-
 @Composable
-fun HomeScreenContent(modifier: Modifier, viewModel: HomeScreenViewModel) {
+fun HomeScreenContent(
+    showDetailRestaurantPage: (restaurantAddress: String) -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: HomeScreenViewModel
+) {
+    // For Tab row
+//    viewModel.setMoodState(MoodState.Bored)
+//    viewModel.getRestaurantList(viewModel.moodState.collectAsState().value)
     val restaurants by viewModel.restaurantList.collectAsState()
-    viewModel.getRestaurantList(MoodState.Happy.name.lowercase())
+    val selectedCategory by viewModel.moodState.collectAsState()
     Column(
-        modifier = modifier,
+        modifier = modifier.padding(top = 8.dp, start = 8.dp, end = 8.dp)
     ) {
         Text(
             "Top Picks",
-            style = MaterialTheme.typography.displaySmall,
+            style = MaterialTheme.typography.titleLarge,
             fontWeight = FontWeight.Bold,
             fontFamily = interFamily,
             color = MaterialTheme.colorScheme.onBackground
         )
         Spacer(modifier = Modifier.height(16.dp))
-        LazyRow(
-            horizontalArrangement = Arrangement.spacedBy(8.dp),
-        ) {
-            items(fakeMood()) { item ->
-                Text(
-                    item,
-                    Modifier
-                        .clip(RoundedCornerShape(16.dp))
-                        .background(MaterialTheme.colorScheme.secondaryContainer)
-                        .padding(8.dp),
-                    color = MaterialTheme.colorScheme.onSecondaryContainer
+
+        // Add horizontal pager here
+        LazyRow() {
+            items(fakeMood()) { mood ->
+                MoodCategoryChip(
+                    mood = mood,
+                    isSelected = selectedCategory == mood,
+                    onSelectedCategoryChanged = {
+                        viewModel.onSelectedCategoryChanged(it)
+                    },
+                    onExecuteSearch = viewModel::getRestaurantList,
                 )
             }
         }
+
         Spacer(modifier = Modifier.height(16.dp))
-        LazyVerticalGrid(
-            columns = GridCells.Fixed(2),
-            verticalArrangement = Arrangement.spacedBy(16.dp),
-            horizontalArrangement = Arrangement.spacedBy(16.dp)
-        ) {
-            (items(restaurants) { item ->
-                RestaurantCard(restaurant = item)
-            })
+        if (restaurants.isEmpty()) {
+            Text(
+                "Loading...",
+                style = MaterialTheme.typography.bodyLarge,
+                fontWeight = FontWeight.Bold,
+                fontFamily = interFamily,
+                color = MaterialTheme.colorScheme.onBackground
+            )
+        } else {
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(2),
+                verticalArrangement = Arrangement.spacedBy(16.dp),
+                horizontalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+                items(restaurants) { restaurant ->
+                    RestaurantCard(restaurant = restaurant,
+                        showDetailRestaurantPage = { showDetailRestaurantPage(restaurant.address) })
+                }
+            }
         }
+
     }
 }
 
-
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun RestaurantCard(
-    restaurant: Restaurant, //onClickToDetailL: () -> Unit
+    restaurant: Restaurant, showDetailRestaurantPage: (restaurant: Restaurant) -> Unit
 ) {
-    Card {
+    Card(onClick = { showDetailRestaurantPage(restaurant) }) {
         Column(
             modifier = Modifier.background(MaterialTheme.colorScheme.primaryContainer)
         ) {
-
             AsyncImage(
                 model = restaurant.imageUrl,
                 contentDescription = "restaurant",
@@ -92,18 +104,15 @@ fun RestaurantCard(
                     .fillMaxWidth()
                     .height(125.dp)
             )
-
-            restaurant.name?.let {
-                Text(
-                    text = it,
-                    modifier = Modifier.padding(8.dp),
-                    maxLines = 1,
-                    fontFamily = interFamily,
-                    style = MaterialTheme.typography.titleMedium,
-                    fontWeight = FontWeight.Bold,
-                    color = MaterialTheme.colorScheme.onPrimaryContainer
-                )
-            }
+            Text(
+                text = restaurant.name ?: "",
+                modifier = Modifier.padding(8.dp),
+                maxLines = 1,
+                fontFamily = interFamily,
+                style = MaterialTheme.typography.titleMedium,
+                fontWeight = FontWeight.Bold,
+                color = MaterialTheme.colorScheme.onPrimaryContainer
+            )
             Row(
                 Modifier
                     .fillMaxWidth()
